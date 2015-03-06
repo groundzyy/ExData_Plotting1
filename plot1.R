5a9a4118306121c9364132aaa3c19b2f5172ea98
groundzyy <study@zhengyiyu.com>
2015-03-06 15:06:58 -0500

    update plot1-4.r and corresponding 4 png files

---
+++ plot1.R
@@ -0,0 +1,10 @@
+data <- read.table("./household_power_consumption.txt", header = TRUE, nrows=70000, sep=";", na.strings="?", colClasses=c("character","character", rep("numeric",7)))
+data$Date <- paste(data$Date, data$Time,sep = " ")
+data$Date <- strptime(data$Date, format="%d/%m/%Y %H:%M:%OS")
+start <- strptime("2007-02-01 00:00:00", format="%Y-%m-%d  %H:%M:%OS")
+end <- strptime("2007-02-03 00:00:00", format="%Y-%m-%d  %H:%M:%OS")
+filter <- which(data$Date >= start & data$Date < end)
+data <- data[filter,]
+png(filename = "plot1.png", width=480, height=480, units="px")
+hist(data$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
